863548940d45a606cfc728507d33de5eb2c5b3ba
Nikkolla <arebanikko@gmail.com>
2023-02-14 05:35:07 +0400

    form control

--- src/pages/formcontrol.tsx
+++ src/pages/formcontrol.tsx
@@ -1,13 +1,21 @@
-import { TextField } from "@mui/material";
+import { FormHelperText, Input, InputLabel, TextField } from "@mui/material";
+import { FormControl as Form } from '@mui/material';
+
+
 
 export default function FormControl() {
   return (
-    <div>
-      <div className="text-black ">
+    <div className="w-full min-h-screen h-full">
+      <div className="min-h-[80vh] h-full w-full text-black flex justify-center items-center flex-col">
         Form Control Example //
-        <code>
+        <div className="flex mt-20 gap-6">
+        <Form>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+</Form>
         <TextField id="filled-basic" label="Filled" variant="filled" />
-        </code>
+      </div>
       </div>
     </div>
   );
